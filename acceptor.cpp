c5fb0b533561a04c6ae38d4b53221f36f18571ee
Narciostyx <2091278275@qq.com>
2026-05-08 18:07:17 +0800

    Now only allow the loop-back address to access.

--- acceptor.cpp
+++ acceptor.cpp
@@ -9,13 +9,13 @@ project::Acceptor::Acceptor(int port) {
     addr.sin_port = htons(port);
 
     int opt = 1;
-    setsockopt(listen_fd_, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-    if (bind(listen_fd_, (sockaddr*)&addr, sizeof(addr)))
+    ::setsockopt(listen_fd_, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    if (::bind(listen_fd_, (sockaddr*)&addr, sizeof(addr)))
     {
         LOG_ERR("Bind to the specific port " + std::to_string(port) + " failed:" + strerror(errno));
         exit(exit_code = 3);
     }
-    if (listen(listen_fd_, SOMAXCONN))
+    if (::listen(listen_fd_, SOMAXCONN))
     {
         LOG_ERR("Listen on the specific port " + std::to_string(port) + " failed:" + strerror(errno));
         exit(exit_code = 3);
@@ -35,11 +35,24 @@ int project::Acceptor::get_fd() const {
 // 接受新连接
 int project::Acceptor::accept() {
     sockaddr_in client;
-    socklen_t client_len = sizeof(client);;
+    socklen_t client_len = sizeof(client);
     char ip[INET_ADDRSTRLEN];
 
     int fd = ::accept(listen_fd_, (sockaddr*)&client, &client_len);
+    if (fd < 0) {
+        return -1;
+    }
+
     ::inet_ntop(AF_INET, &client.sin_addr, ip, sizeof(ip));
+
+    // IP限制逻辑：目前仅允许本地回环地址 127.0.0.1 访问
+    std::string client_ip(ip);
+    if (client_ip != "127.0.0.1") {
+        LOG_WARN("Blocked connection from IP " + client_ip + ": only 127.0.0.1 is allowed.");
+        ::close(fd);
+        return -1;
+    }
+
     LOG_INFO(std::format("Connect to the host {}:{} with fd {}.", ip, ::ntohs(client.sin_port), fd));
     return fd;
 }
